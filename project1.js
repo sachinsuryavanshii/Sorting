7ef569d7da6d9772ad7f8e62d676aa92aa2cd785
Sachin V. Suryavanshi <127820125+sachinsuryavanshii@users.noreply.github.com>
2024-11-04 12:12:49 +0530

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,7 @@
+function sortDemo() {
+	arr.sort()
+	console.log(arr);
+}
+const arr = [3, 1, 8, 5, 2, 1];
+sortDemo();
+
---
+++ project2.js
@@ -0,0 +1,6 @@
+function sortDemo() {
+	arr.sort()
+	console.log(arr);
+}
+const arr = [-3, 1, 10, 12, 1];
+sortDemo();
